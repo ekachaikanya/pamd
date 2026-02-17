189ea719bce6e55d8bc9f58bf6a50c6aa8475573
ekachai.k <ekachai.k@bu.ac.th>
2026-02-17 20:34:18 +0700

    refactor navigation and update document title in user.php

--- php/src/nav.php
+++ php/src/nav.php
@@ -1,8 +1,5 @@
-<nav>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Blog</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container">
+    <a class="navbar-brand" href="#">Ekachaik System</a>
+  </div>
 </nav>
